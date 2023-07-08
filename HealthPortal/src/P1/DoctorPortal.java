b99f8b26334ff7f8cbf8953d61c7a079458cae41
rhoda-asu <138174762+rhoda-asu@users.noreply.github.com>
2023-07-08 01:18:39 -0700

    Add files via upload
    lmk if i need to fix up anything I can't run javafx on my computer atm

--- HealthPortal/src/P1/DoctorPortal.java
+++ HealthPortal/src/P1/DoctorPortal.java
@@ -4,70 +4,99 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class DoctorPortal {
-	private JButton SavaButton;
-	private JButton patientHistoryButton;
-	DoctorPortal(){
-		JFrame frame = new JFrame();
-		SavaButton = new JButton("Save");
-		patientHistoryButton = new JButton("Patient History");
-		JLabel PhysicalTest = new JLabel("Physical Test");
-		
-		JTextField EarField = new JTextField();
-		JTextField MedicationField = new JTextField();
-		JTextField VisionField = new JTextField();
-		JTextField ThroutField = new JTextField();
-		JTextField SkinField = new JTextField();
-		JTextField LesionsField = new JTextField();
-		
-		JLabel MedicationL = new JLabel("Medication:");
-		JLabel EarL = new JLabel("Ear:");
-		JLabel VisionL = new JLabel("Vision:");
-		JLabel ThroutL = new JLabel("Throut:");
-		JLabel SkinL = new JLabel("Skin:");
-		JLabel LesionsL = new JLabel("Lesions:");
-		
-		SavaButton.setBounds(650,600,100,50);
-		patientHistoryButton.setBounds(500,600,125,50);
-		PhysicalTest.setBounds(350,10,200,50);
-		
-		MedicationField.setBounds(150,65,300,50);
-		EarField.setBounds(150,150,300,50);
-		VisionField.setBounds(150,250,300,50);
-		ThroutField.setBounds(150,350,300,50);
-		SkinField.setBounds(150,450,300,50);
-		LesionsField.setBounds(150,550,300,50);
-		
-		MedicationL.setBounds(10,50,200,50);
-		EarL.setBounds(10,150,200,50);
-		VisionL.setBounds(10,250,200,50);
-		ThroutL.setBounds(10,350,200,50);
-		SkinL.setBounds(10,450,200,50);
-		LesionsL.setBounds(10,550,200,50);
-		
-		frame.add(SavaButton);
-		frame.add(patientHistoryButton);
-		frame.add(PhysicalTest);
-		frame.add(MedicationField);
-		frame.add(EarField);
-		frame.add(VisionField);
-		frame.add(ThroutField);
-		frame.add(SkinField);
-		frame.add(LesionsField);
-		
-		frame.add(MedicationL);
-		frame.add(EarL);
-		frame.add(VisionL);
-		frame.add(ThroutL);
-		frame.add(SkinL);
-		frame.add(LesionsL);
-	
-		
-		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(800,700);
-		frame.setLayout(null);
-		frame.setVisible(true);
-	}
+    private JButton saveButton;
+    private JButton patientHistoryButton;
+
+    DoctorPortal() {
+        JFrame frame = new JFrame();
+        saveButton = new JButton("Save");
+        patientHistoryButton = new JButton("Patient History");
+        JLabel physicalTest = new JLabel("Physical Test");
+        JTextField medicationField = new JTextField();
+        JTextField earField = new JTextField();
+        JTextField visionField = new JTextField();
+        JTextField throatField = new JTextField();
+        JTextField skinField = new JTextField();
+        JTextField lesionsField = new JTextField();
+
+        JLabel medicationLabel = new JLabel("Medication:");
+        JLabel earLabel = new JLabel("Ear:");
+        JLabel visionLabel = new JLabel("Vision:");
+        JLabel throatLabel = new JLabel("Throat:");
+        JLabel skinLabel = new JLabel("Skin:");
+        JLabel lesionsLabel = new JLabel("Lesions:");
+
+        saveButton.setBounds(650, 600, 100, 50);
+        patientHistoryButton.setBounds(500, 600, 125, 50);
+        physicalTest.setBounds(350, 10, 200, 50);
+
+        medicationField.setBounds(150, 65, 300, 50);
+        earField.setBounds(150, 150, 300, 50);
+        visionField.setBounds(150, 250, 300, 50);
+        throatField.setBounds(150, 350, 300, 50);
+        skinField.setBounds(150, 450, 300, 50);
+        lesionsField.setBounds(150, 550, 300, 50);
+
+        medicationLabel.setBounds(10, 50, 200, 50);
+        earLabel.setBounds(10, 150, 200, 50);
+        visionLabel.setBounds(10, 250, 200, 50);
+        throatLabel.setBounds(10, 350, 200, 50);
+        skinLabel.setBounds(10, 450, 200, 50);
+        lesionsLabel.setBounds(10, 550, 200, 50);
+
+        saveButton.addActionListener(e -> {
+            String medication = medicationField.getText();
+            String ear = earField.getText();
+            String vision = visionField.getText();
+            String throat = throatField.getText();
+            String skin = skinField.getText();
+            String lesions = lesionsField.getText();
+
+            String data = "Medication: " + medication + "\n" +
+                    "Ear: " + ear + "\n" +
+                    "Vision: " + vision + "\n" +
+                    "Throat: " + throat + "\n" +
+                    "Skin: " + skin + "\n" +
+                    "Lesions: " + lesions;
+
+            try {
+                FileWriter writer = new FileWriter("data.txt");
+                writer.write(data);
+                writer.close();
+                System.out.println("Data saved to file.");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        frame.add(saveButton);
+        frame.add(patientHistoryButton);
+        frame.add(physicalTest);
+        frame.add(medicationField);
+        frame.add(earField);
+        frame.add(visionField);
+        frame.add(throatField);
+        frame.add(skinField);
+        frame.add(lesionsField);
+
+        frame.add(medicationLabel);
+        frame.add(earLabel);
+        frame.add(visionLabel);
+        frame.add(throatLabel);
+        frame.add(skinLabel);
+        frame.add(lesionsLabel);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 700);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+/*
+    public static void main(String[] args) {
+        new DoctorPortal();
+    }*/
 }
